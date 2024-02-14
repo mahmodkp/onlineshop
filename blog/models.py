7370cadb934fbd68aca47ef21f6ec20b6013c867
Mahmood KP <m.karamipour@gmail.com>
2024-02-15 00:06:58 +0330

    Blog model created.

--- blog/models.py
+++ blog/models.py
@@ -3,29 +3,29 @@ from accounts.models import CustomUser
 # Create your models here.
 
 
-def get_default_product_category():
+def get_default_article_category():
     return Category.objects.get_or_create(name="unknown")[0]
 
 
 def category_image_path(instance, filename):
-    return f"products/category/images/{instance.name}/{filename}"
+    return f"blog/category/images/{instance.name}/{filename}"
 
 
-def product_image_path(instance, filename):
-    return f"products/images/{instance.name}/{filename}"
+def article_image_path(instance, filename):
+    return f"blog/images/{instance.name}/{filename}"
 
 
 def gallery_image_path(instance, filename):
-    return f"products/gallery/images/{instance.name}/{filename}"
+    return f"blog/gallery/images/{instance.name}/{filename}"
 
 
 def gallery_vodeo_path(instance, filename):
-    return f"products/gallery/videos/{instance.name}/{filename}"
+    return f"blog/gallery/videos/{instance.name}/{filename}"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(null=True,blank=True)
+    description = models.TextField(null=True, blank=True)
     image = models.ImageField(
         upload_to=category_image_path, blank=True, null=True)
     is_active = models.BooleanField(default=True)
@@ -34,20 +34,18 @@ class Category(models.Model):
         return self.name
 
 
-class Product(models.Model):
+class Article(models.Model):
     category = models.ForeignKey(
         Category,
-        related_name="products",
-        on_delete=models.SET(get_default_product_category),
+        related_name="articles",
+        on_delete=models.SET(get_default_article_category),
     )
-    name = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=12, decimal_places=2)
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-    description = models.TextField(blank = True,null=True)
-    image = models.ImageField(upload_to=product_image_path, blank=True,null=True)
-    quantity = models.IntegerField(default=1)
+    title = models.CharField(max_length=200)
+    text = models.TextField(blank=True, null=True)
+    image = models.ImageField(
+        upload_to=article_image_path, blank=True, null=True)
     is_active = models.BooleanField(default=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -55,27 +53,25 @@ class Product(models.Model):
         ordering = ("-created_at",)
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class ImageGallery(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='gallery_image_path', null=True)
     is_active = models.BooleanField(default=True)
 
 
 class VideoGallery(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     video = models.FileField(upload_to="gallery_vodeo_path",
                              null=True)
     is_active = models.BooleanField(default=True)
 
 
 class Comment(models.Model):
-    product = models.ForeignKey(
-        Product, on_delete=models.CASCADE, related_name='product_comments')
-    user = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, related_name='product_comments')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='article_comments')
     text = models.TextField()
     is_active = models.BooleanField(default=True)
     is_confirmed = models.BooleanField(default=False)
--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     # Apps
     'accounts',
     'products',
+    'blog',
 
 
 
