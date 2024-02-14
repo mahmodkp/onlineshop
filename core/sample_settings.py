836c99447f0de4839d5e0bcbd3ef62cbf98df891
Mahmood KP <m.karamipour@gmail.com>
2024-02-14 23:23:31 +0330

    change the settings

--- core/sample_settings.py
+++ core/sample_settings.py
@@ -5,3 +5,15 @@ SECRET_KEY = ''
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
